881a0bb3d6c25acf801809e3e908bef9f5f23af3
Nicholas Roethel <roetheln@gmail.com>
2019-11-22 11:50:07 -0800

    got some FAT to work

--- diskinfo.c
+++ diskinfo.c
@@ -25,7 +25,9 @@ struct __attribute__((__packed__)) superblock_t {
 int main(int argc, char* argv[])	{
 
 	//open the file
-	int fd = open(argv[1], O_RDWR);
+	int fd = open("test.img", O_RDWR);
+	//int fd = open(argv[1], O_RDWR);
+
 
 	if (fd == -1)	{
 		printf("error opening test.img\n");
